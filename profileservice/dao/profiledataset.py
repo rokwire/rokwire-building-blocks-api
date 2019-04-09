7af7d16647bd9a4965389598c933c3ce476b5e89
Kim <ywkim@illinois.edu>
2019-04-09 15:37:36 -0500

    created load image method

--- profileservice/dao/profiledataset.py
+++ profileservice/dao/profiledataset.py
@@ -14,13 +14,38 @@ class ProfileDataset:
         self.uuid = None
         self.device_id = None
         self.username = None
+        self.file_descriptors = None
+        self.image_uri = None
 
-        profile_info = injson["profile"]
-        self.set_uuid(str(uuid.uuid4()))
-        self.set_piiid_dataset(profile_info["pii"])
-        self.set_non_piiid_dataset(profile_info["non-pii"])
-        self.set_username(profile_info['username'])
-        self.set_device_id(profile_info['device_id'])
+        try:
+            self.set_uuid(injson["uuid"])
+        except:
+            self.set_uuid(str(uuid.uuid4()))
+
+        try:
+            self.set_piiid_dataset(injson["piiidDataset"])
+        except:
+            pass
+        try:
+            self.set_non_piiid_dataset(injson["nonPiiidDataset"])
+        except:
+            pass
+        try:
+            self.set_username(injson['username'])
+        except:
+            pass
+        try:
+            self.set_device_id(injson['device_id'])
+        except:
+            pass
+        try:
+            self.set_file_descriptors(injson['file_descriptors'])
+        except:
+            pass
+        try:
+            self.set_image_uri(injson['image_uri'])
+        except:
+            pass
 
     def get_username(self):
         return self.username
@@ -50,9 +75,18 @@ class ProfileDataset:
     def set_piiid_dataset(self, pii):
         if (pii != None):
             self.piiidDataset = PiiidDataset()
-            new_id = ObjectId()
-            new_id = str(new_id)
-            self.piiidDataset.set_id(new_id)
+            try:
+                self.piiidDataset.set_id(pii["id"])
+            except:
+                new_id = ObjectId()
+                new_id = str(new_id)
+                self.piiidDataset.set_id(new_id)
+
+            try:
+                self.piiidDataset.set_pii_uuid(pii["pii_uuid"])
+            except:
+                pass
+
             try:
                 self.piiidDataset.set_lastname(pii['lastname'])
             except Exception as e:
@@ -80,10 +114,35 @@ class ProfileDataset:
     def set_non_piiid_dataset(self, non_pii):
         if (non_pii != None):
             self.nonPiiidDataset = NonPiiidDataset()
-            new_id = ObjectId()
-            new_id = str(new_id)
-            self.nonPiiidDataset.set_id(new_id)
+            try:
+                self.nonPiiidDataset.set_id(non_pii["id"])
+            except:
+                new_id = ObjectId()
+                new_id = str(new_id)
+                self.nonPiiidDataset.set_id(new_id)
+
+            try:
+                self.nonPiiidDataset.set_non_pii_uuid(non_pii["non_pii_uuid"])
+            except:
+                pass
+
             try:
                 self.nonPiiidDataset.set_hobby(non_pii["hobby"])
             except Exception as e:
                 pass
+
+    def add_file_descriptor(self, file_descriptor):
+        if (file_descriptor != None):
+            self.get_file_descriptors().append(file_descriptor)
+
+    def set_file_descriptors(self, file_descriptors):
+        self.file_descriptors = file_descriptors
+
+    def get_file_descriptors(self):
+        return self.file_descriptors
+
+    def set_image_uri(self, image_uri):
+        self.image_uri = image_uri
+
+    def get_image_uri(self):
+        return self.image_uri
--- profileservice/restservice/rest_service.py
+++ profileservice/restservice/rest_service.py
@@ -3,32 +3,34 @@ author: Yong Wook Kim
 created 2019 Apr 5
 """
 #!flask/bin/python
-import pymongo
+import hashlib
 import logging
+import tempfile
+import urllib
+from urllib.request import urlretrieve
+
 import flask
 import os
 import json
+import shutil
+import profileservice.configs as cfg
 
+from mimetypes import MimeTypes
 from bson import ObjectId
-from flask import jsonify
+from flask import jsonify, flash, redirect
 from pymongo import MongoClient
 from flask import make_response
-from profileservice.dao.profiledataset import ProfileDataset
-
 from flask import request
 from bson.json_util import dumps
-
+from profileservice.dao.profiledataset import ProfileDataset
+from profileservice.dao.filedescriptor import FileDescriptor
 
 app = flask.Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
-mongo_url = os.getenv('MONGODB_URI', 'localhost:27017')
-db_name = os.getenv('DB_NAME', 'profiledb')
-coll_name = os.getenv('DB_COLLECTION', 'ProfileDataset')
-
-client = pymongo.MongoClient(mongo_url, connect=False)
-db = client[db_name]
-db.collection = db[coll_name]
+client = MongoClient(cfg.profile_mongo_url, connect=False)
+db = client[cfg.profile_db_name]
+db.collection = db[cfg.profile_db_coll_name]
 __logger = logging.getLogger("rokwire-building-blocks-api")
 
 
@@ -38,11 +40,9 @@ rest service for root directory
 @app.route('/profiles', methods = ['GET', 'POST'])
 def root_dir():
     if request.method == 'GET':
-        db_data = db.collection.find({}, {'_id': False})
+        db_data = db.collection.find({}) #db.collection.find({}, {'_id': False})
         data_list = list(db_data)
-        data_dump = dumps(data_list)
-        out_json = make_response(data_dump)
-        out_json.mimetype = 'application/json'
+        out_json = construct_json_from_query_list(data_list)
         logging.debug("list all profiles")
 
         return out_json
@@ -50,12 +50,9 @@ def root_dir():
     if request.method == 'POST':
         in_json = request.get_json()
         profile = ProfileDataset(in_json)
-        dataset, id = insert_profile_to_mongodb(mongo_url, profile)
-
-        data_dump = dumps(dataset)
-        out_json = make_response(data_dump)
-        out_json.mimetype = 'application/json'
-        logging.debug("list all profiles")
+        dataset, id = insert_profile_to_mongodb(profile)
+        out_json = construct_json_from_query_list(dataset)
+        logging.debug("new profiles added: " + str(id))
 
         msg = "data has been posted: " + str(id)
         logging.debug(msg)
@@ -67,29 +64,19 @@ provide profile information by profile id or remove it
 """
 @app.route('/profiles/<profileid>', methods = ['GET', 'DELETE'])
 def deal_profile_id(profileid):
-    out_json = None
-    is_profile_id = True
-
     if profileid != None:
-        # check if the input variable is either object id or user name
-        try:
-            id = ObjectId(profileid)
-        except:
-            is_profile_id = False
+        is_profile_id = check_if_objectid(profileid)
 
         # query using either profile id or user name
         if (is_profile_id):
             id = ObjectId(profileid)
-            db_data = db.collection.find({'_id':id})
+            db_data = query_dataset_by_objectid(id)
         else:
-            db_data = db.collection.find({'username': profileid})
+            db_data = query_dataset('username', profileid)
 
-        # construct output json
         data_list = list(db_data)
         if len(data_list) > 0:
-            data_dump = dumps(data_list)
-            out_json = make_response(data_dump)
-            out_json.mimetype = 'application/json'
+            out_json = construct_json_from_query_list(data_list)
 
             if request.method == 'GET':
                 msg = "request profile information: " + str(profileid)
@@ -107,23 +94,185 @@ def deal_profile_id(profileid):
             return out_json
         else:
             return not_found()
+    else:
+        return not_found()
 
+"""
+provide profile information by profile id or remove it
+"""
+@app.route('/profiles/<profileid>/uploadImage', methods = ['POST'])
+def upload_profile_image(profileid):
+    if request.method == 'POST':
+        dataset = get_dataset_from_objectid(profileid)
+        if dataset is None:
+            return not_found()
+        else:
+            # create FileDescriptor for uploaded file
+            if 'file' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
+
+            file = request.files['file']
+
+            if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+
+            fd = create_file_descriptor(cfg.profile_rest_storage, file)
+            file_descriptors = dataset.get_file_descriptors()
+            if file_descriptors is None:
+                file_descriptors = []
+            file_descriptors.append(fd)
+            dataset.set_file_descriptors(file_descriptors)
+            dataset.set_image_uri(fd.dataURL)
+
+            result, dataset = update_dataset_in_mongo(profileid, dataset)
+
+            if (result):
+                out_json = construct_json_from_query_list(dataset)
+                msg = "image has been posted to: " + str(profileid)
+                logging.debug(msg)
+
+                return out_json
+            else:
+                return bad_request()
+
+"""
+convert mongodb query result to profile object
+"""
+def get_dataset_from_objectid(profileid):
+    is_profile_id = check_if_objectid(profileid)
+    if is_profile_id:
+        id = ObjectId(profileid)
+        db_data = query_dataset_by_objectid(id)
+        data_list = list(db_data)
+        if len(data_list) > 0:
+            data_dump = dumps(data_list)
+            data_dump = data_dump[:-1]
+            data_dump = data_dump[1:]
+            json_load = json.loads(data_dump)
+            dataset = ProfileDataset(json_load)
+
+            return dataset
+        else:
+            return None
+    else:
+        return None
+
+"""
+check if the query string is objectid
+"""
+def check_if_objectid(query_str):
+    is_objectid = True
+    try:
+        id = ObjectId(query_str)
+    except:
+        is_objectid = False
+
+    return is_objectid
+
+"""
+query dataset using object id
+"""
+def query_dataset_by_objectid(objectid):
+    return db.collection.find({'_id':objectid})
+
+"""
+qyery dataset using field
+"""
+def query_dataset(fld, query_str):
+    return db.collection.find({fld: query_str})
+
+"""
+construct json from mongo query
+"""
+def construct_json_from_query_list(data_list):
+    data_dump = dumps(data_list)
+    out_json = make_response(data_dump)
+    out_json.mimetype = 'application/json'
+
+    return out_json
 
 """
 insert profile json to mognodb
 """
-def insert_profile_to_mongodb(mongo_url, indataset):
-    client = MongoClient(mongo_url, 27017)
-    db = client['profiledb']
-    coll = db.ProfileDataset
-
+def insert_profile_to_mongodb(indataset):
     dataset = json.dumps(indataset, default=lambda x: x.__dict__)
     dataset = json.loads(dataset)
 
-    id = coll.insert(dataset)
+    id = db.collection.insert(dataset)
 
     return dataset, id
 
+"""
+update profile dataset in mongodb
+"""
+def update_dataset_in_mongo(profileid, datasetobj):
+    dataset = json.dumps(datasetobj, default=lambda x: x.__dict__)
+    dataset = json.loads(dataset)
+    id = ObjectId(profileid)
+    result = db.collection.update_one({'_id':id}, {"$set": dataset}, upsert=False)
+
+    return result.acknowledged, dataset
+
+"""
+create FileDescriptor object
+"""
+def create_file_descriptor(data_repo_dir, file):
+    filename = file.filename
+    fd = FileDescriptor()
+    new_id = ObjectId()
+    new_id = str(new_id)
+    fd.set_id(new_id)
+
+    # create folder
+    levels = 2
+    path = ''
+    i = 0
+    while (i < levels * 2 and len(new_id) >= i + 2):
+        path = path + new_id[i: i + 2] + os.sep
+        i = i + 2
+
+    if (len(new_id) > 0):
+        path = path + new_id + os.sep
+
+    path = data_repo_dir + os.sep + path
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    # save file to path
+    file.save(os.path.join(path, filename))
+
+    # if os.path.isfile(filename):
+    #     shutil.copy(filename, path)
+
+    saved_file = path + os.path.basename(filename)
+    fd.set_data_url('file:' + os.sep + saved_file)
+
+    fd.set_filename(os.path.basename(filename))
+
+    mime = MimeTypes()
+    mime_type = mime.guess_type(urllib.request.pathname2url(filename))
+    if mime_type[0] == None:
+        fd.set_mime_type('application/octet-stream')
+    else:
+        fd.set_mime_type(mime_type[0])
+
+    file_stat = os.stat(saved_file)
+    fd.set_size(file_stat.st_size)
+
+    hash_md5 = hashlib.md5()
+    with open(saved_file, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    fd.set_md5sum(hash_md5.hexdigest())
+
+    return fd
+
+"""
+error handlers
+"""
 @app.errorhandler(404)
 def not_found(error=None):
     message = {
@@ -132,6 +281,17 @@ def not_found(error=None):
     }
     resp = jsonify(message)
     resp.status_code = 404
+
+    return resp
+
+@app.errorhandler(400)
+def bad_request(error=None):
+    message = {
+            'status': 400,
+            'message': 'Bad Request: ' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 400
 
     return resp
 
