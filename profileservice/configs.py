d96887d381f30c2bc56d8613293ef030f49919e5
Sandeep Puthanveetil Satheesan <sandeep.ps4@gmail.com>
2019-05-17 16:00:38 -0500

    Update profileservice/configs.py

--- profileservice/configs.py
+++ profileservice/configs.py
@@ -1,5 +1,5 @@
 # this has to be have the file path to the folder for saving image files and other necessary files from rest service
-PROFILE_REST_STORAGE = 'C:\\rest'
+PROFILE_REST_STORAGE = ''
 
 PROFILE_MONGO_URL = 'localhost:27017'
 PROFILE_DB_NAME = 'profiledb'
