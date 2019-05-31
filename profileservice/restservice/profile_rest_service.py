dad56faa0af45c6ec5b7026d3842b109462e9671
Kim <ywkim@illinois.edu>
2019-05-31 11:53:11 -0500

    fixed import

--- profileservice/restservice/profile_rest_service.py
+++ profileservice/restservice/profile_rest_service.py
@@ -14,6 +14,7 @@ import profileservice.restservice.utils.datasetutils as datasetutils
 from bson import ObjectId
 from flask import flash, redirect, jsonify, make_response, request
 
+from profileservice import middleware
 from profileservice.dao.pii_data import pii_data
 from profileservice.dao.non_pii_data import non_pii_data
 from profileservice.restservice.utils.otherutils import create_file_descriptor
