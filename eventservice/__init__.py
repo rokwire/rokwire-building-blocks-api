e4a388b305786f4c1ef319bd1211c6aca0f80b56
span14 <span14@illinois.edu>
2019-06-26 15:52:42 -0500

    fix some typos

--- eventservice/__init__.py
+++ eventservice/__init__.py
@@ -6,7 +6,7 @@ from . import event_rest_service
 
 def create_app():
     # create and configure the app
-    app = Flask(__name__, root_path='eventservice/', )
+    app = Flask(__name__, root_path='eventservice/')
     app.url_map.strict_slashes = False
     app.register_blueprint(event_rest_service.bp)
     app.config.from_pyfile('config.py', silent=True)
