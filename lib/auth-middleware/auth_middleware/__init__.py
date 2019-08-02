e3073ecf34017b0907af42d1bb0aeee8d387d2ef
Frank Henard <fhena2@illinois.edu>
2019-08-02 15:37:24 -0500

    comment google authentication in middleware since we're no longer using it
    comment google auth to indicate that we're no longer using it.  I'm not deleting it in case someone wants to look at it later.  Feel free to delete if necessary

--- lib/auth-middleware/auth_middleware/__init__.py
+++ lib/auth-middleware/auth_middleware/__init__.py
@@ -91,42 +91,42 @@ def use_security_token_auth(func):
     return func
 
 
-def authenticate_google():
-    from google.oauth2 import id_token
-    from google.auth.transport import requests as google_auth_requests
+# def authenticate_google():
+#     from google.oauth2 import id_token
+#     from google.auth.transport import requests as google_auth_requests
 
-    _id_token = request.headers.get('Id-Token')
-    if not _id_token:
-        logger.warning("Request missing Id-Token header")
-        abort(401)
-    client_platform = request.headers.get('Client-Platform')
-    if not client_platform:
-        logger.warning("Request missing Client-Platform header")
-        abort(401)
-    if client_platform == 'android':
-        client_id = '995345377706-jo1h6i34bm6k2gce2018an17iohe2ouf.apps.googleusercontent.com'
-    elif client_platform == 'ios':
-        client_id = '995345377706-8rti0kckia00gnv0kn56btrcbgour92a.apps.googleusercontent.com'
-    else:
-        logger.warning("unrecognized client platform of %s" % client_platform)
-        abort(401)
-    try:
-        id_info = id_token.verify_oauth2_token(
-            _id_token,
-            google_auth_requests.Request(),
-            client_id,
-        )
-    except ValueError as ve:
-        logger.warning("ValueError on token verify. Message = %s" % ve)
-        abort(401)
-    if not id_info:
-        logger.warning("token not verified")
-        abort(401)
-    if id_info['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
-        logger.warning("invalid iss of %s" % id_info['iss'])
-        abort(401)
-    if id_info['hd'] != 'illinois.edu':
-        logger.warning("unrecognized host domain of %s" % id_info['hd'])
-        abort(401)
-    request.user_token_data = id_info
-    return
+#     _id_token = request.headers.get('Id-Token')
+#     if not _id_token:
+#         logger.warning("Request missing Id-Token header")
+#         abort(401)
+#     client_platform = request.headers.get('Client-Platform')
+#     if not client_platform:
+#         logger.warning("Request missing Client-Platform header")
+#         abort(401)
+#     if client_platform == 'android':
+#         client_id = '995345377706-jo1h6i34bm6k2gce2018an17iohe2ouf.apps.googleusercontent.com'
+#     elif client_platform == 'ios':
+#         client_id = '995345377706-8rti0kckia00gnv0kn56btrcbgour92a.apps.googleusercontent.com'
+#     else:
+#         logger.warning("unrecognized client platform of %s" % client_platform)
+#         abort(401)
+#     try:
+#         id_info = id_token.verify_oauth2_token(
+#             _id_token,
+#             google_auth_requests.Request(),
+#             client_id,
+#         )
+#     except ValueError as ve:
+#         logger.warning("ValueError on token verify. Message = %s" % ve)
+#         abort(401)
+#     if not id_info:
+#         logger.warning("token not verified")
+#         abort(401)
+#     if id_info['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+#         logger.warning("invalid iss of %s" % id_info['iss'])
+#         abort(401)
+#     if id_info['hd'] != 'illinois.edu':
+#         logger.warning("unrecognized host domain of %s" % id_info['hd'])
+#         abort(401)
+#     request.user_token_data = id_info
+#     return
