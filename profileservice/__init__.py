c212b8e537f1f1bf938098b34047acb58167bc80
Kim <ywkim@illinois.edu>
2019-08-05 15:52:00 -0500

    modified init.py

--- profileservice/__init__.py
+++ profileservice/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
+from . import profile_rest_service
 from . import db
-from . import event_rest_service
 
 def create_app():
     # create and configure the app
-    app = Flask(__name__, root_path='eventservice/')
+    app = Flask(__name__, root_path='profileservice/')
     app.url_map.strict_slashes = False
-    app.register_blueprint(event_rest_service.bp)
-    app.config.from_pyfile('config.py', silent=True)
+    app.register_blueprint(profile_rest_service.bp)
+    app.config.from_pyfile('configs.py', silent=True)
     # Connect to database
     db.init_app(app)
 
