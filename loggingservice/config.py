7adf73081102102fcf2acacfd2eeaad846ca8199
Kim <ywkim@illinois.edu>
2019-08-13 16:07:28 -0500

    initial push for testing

---
+++ loggingservice/config.py
@@ -0,0 +1,3 @@
+LOGGING_MONGO_URL="mongodb://localhost:27017"
+LOGGING_DB_NAME="loggingdb"
+LOGGING_COLL_NAME="LogginDataset"
---
+++ loggingservice/gunicorn.config.py
@@ -0,0 +1,5 @@
+"""Gunicorn configuration."""
+
+bind = '0.0.0.0:5000'
+
+workers = 4
