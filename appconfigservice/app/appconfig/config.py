df28e2c649e60153dd7f8fbc55a68f34406f4367
xialiao <xialiao@users.noreply.github.com>
2019-08-30 15:04:43 -0500

    AppConfig: Fargate deployment and DocumentDB (#198)
    * Fargate deployment and DocumentDB
    
    * remove sudo from docker cmd

--- appconfigservice/app/appconfig/config.py
+++ appconfigservice/app/appconfig/config.py
@@ -1,4 +1,6 @@
-APP_CONFIG_MONGO_URL = 'mongodb://localhost:27017'
+import os
+
+APP_CONFIG_MONGO_URL = os.getenv('APP_CONFIG_MONGO_URL', 'mongodb://localhost:27017')
 APP_CONFIG_DB_NAME = 'app_config_db'
 APP_CONFIG_MAX_POOLSIZE = 100,
 APP_CONFIGS_COLLECTION = 'app_configs'
