e004347d50ccf6dbb371b27950464e9f2c2710ec
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2019-09-16 14:12:52 -0500

    Add Dockerfile, README, config changes to Events Building Block for supporting authentication.

--- eventservice/db.py
+++ eventservice/db.py
@@ -1,17 +1,11 @@
 from flask import current_app, g
 from pymongo.mongo_client import MongoClient
 
-# FIXME! Next 2 lines added for debugging
-EVENT_MONGO_URL = "mongodb://localhost:27017"
-EVENT_DB_NAME = "eventdb"
 
 def get_db():
     if 'db' not in g:
-        # FIXME! next 2 lines need to be uncommented and following 2 removed!
-        # g.client = MongoClient(current_app.config['EVENT_MONGO_URL'])
-        # g.db = g.client.get_database(name=current_app.config['EVENT_DB_NAME'])
-        g.client = MongoClient(EVENT_MONGO_URL)
-        g.db = g.client.get_database(name=EVENT_DB_NAME)
+        g.client = MongoClient(current_app.config['EVENT_MONGO_URL'])
+        g.db = g.client.get_database(name=current_app.config['EVENT_DB_NAME'])
     return g.db
 
 def close_db(e=None):
