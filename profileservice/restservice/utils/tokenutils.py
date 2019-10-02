5ddf492b62ab047e307e9cc17d20e0538a176b5f
Kim <ywkim@illinois.edu>
2019-10-02 13:10:37 -0500

    changed the comments for the code

--- profileservice/restservice/utils/tokenutils.py
+++ profileservice/restservice/utils/tokenutils.py
@@ -3,13 +3,13 @@ def get_id_info_from_token(in_token):
     id_type = 0 # 0 for no id string, 1 for pii data, 2 for non-pii
     id_string = ""
 
-    # check if this is pii token or non-pii token
-    # if there is uin, it is pii
+    # check if the pii token is from campus or from outside the campus
+    # if there is uin, it is from campus
     if 'uiucedu_uin' in in_token:
         id_string = in_token['uiucedu_uin']
         id_type = 1
 
-    # if there is phone number, it is non-pii
+    # if there is phone number, it is from outside the campus
     if 'phoneNumber' in in_token:
         id_string = in_token['phoneNumber']
         id_type = 2
