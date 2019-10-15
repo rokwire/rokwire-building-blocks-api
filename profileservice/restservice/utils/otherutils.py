e77ab469e41b3f5a6fff2b608a0fd0ed4bcf7745
Kim <ywkim@illinois.edu>
2019-10-15 14:27:49 -0500

    changed spacing

--- profileservice/restservice/utils/otherutils.py
+++ profileservice/restservice/utils/otherutils.py
@@ -69,8 +69,9 @@ convert utd time format for rokwire
 """
 def get_current_time_utc():
     currenttime = datetime.utcnow()
-    (formattedtime, micro) = currenttime.strftime('%Y-%m-%dT%H:%M:%S.%f').split('.')
+    formattedtime, micro = currenttime.strftime('%Y-%m-%dT%H:%M:%S.%f').split('.')
     formattedtime = "%s.%03dZ" % (formattedtime, int(micro) / 1000)
+
     return formattedtime
 
 """
