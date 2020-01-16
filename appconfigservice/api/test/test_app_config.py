871a5e4ffc516ccd45e1c25bf94bf9090c752905
Wenjie <wenjzhuu@gmail.com>
2020-01-16 13:25:34 -0600

    updates based on feedbacks

--- appconfigservice/api/test/test_app_config.py
+++ appconfigservice/api/test/test_app_config.py
@@ -21,13 +21,13 @@ def app():
 
 @pytest.fixture
 def client(app):
-    """A test client for the api."""
+    """A test client for the app."""
     return app.test_client()
 
 
 @pytest.fixture
 def runner(app):
-    """A test runner for the api's Click commands."""
+    """A test runner for the app's Click commands."""
     return app.test_cli_runner()
 
 
--- appconfigservice/setup.py
+++ appconfigservice/setup.py
@@ -5,8 +5,6 @@ setup(
     version='1.0.0',
     description='The app config building block',
     package_dir={'': 'api'},
-    # namespace_packages=['api',],
-    # packages=find_packages(),
     packages=find_namespace_packages(where='api'),
     include_package_data=True,
     zip_safe=False,
