c2b60f045387564421d50318c3b873f3ebe013e9
wenjzhuu <wenjzhuu@gmail.com>
2020-06-18 15:25:51 -0500

    frontend updates

--- contributions/catalog/auth.py
+++ contributions/catalog/auth.py
@@ -38,6 +38,31 @@ def register():
         flash(error)
     return render_template('auth/register.html')
 
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
+
 
 
 def check_login(view):
@@ -87,11 +112,11 @@ def login_shibboleth():
     return Redirect(login_url)
 
 
-@bp.route('/login')
-@check_login
-def login():
-    if Config.LOGIN_MODE == "shibboleth":
-        return login_shibboleth()
+# @bp.route('/login')
+# @check_login
+# def login():
+#     if Config.LOGIN_MODE == "shibboleth":
+#         return login_shibboleth()
 
 
 @bp.route('/callback')
@@ -147,18 +172,6 @@ def callback():
             return redirect(url_for("auth.login"))
 
 
-@bp.route('/select-events', methods=['GET', 'POST'])
-@role_required("both")
-def select_events():
-    if request.method == 'POST':
-        event = request.form.get("event")
-        if event == "user":
-            return redirect(url_for("user_events.user_events"))
-        elif event == "source":
-            return redirect(url_for("event.source", sourceId=0))
-        else:
-            return render_template("auth/select-events.html")
-    return render_template("auth/select-events.html")
 
 
 @bp.before_app_request
@@ -175,7 +188,7 @@ def load_logged_in_user_info():
 @role_required('either')
 def logout():
     session.clear()
-    return redirect(url_for('home.home'))
+    return redirect(url_for('contribute.home'))
 
 
 def login_required(view):
