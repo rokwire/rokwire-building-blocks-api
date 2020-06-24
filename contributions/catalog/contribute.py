95ffed3fe56d2fc1958056a76e4ac05af3f8c27e
wenjzhuu <wenjzhuu@gmail.com>
2020-06-24 11:28:29 -0500

    Update contribute.py

--- contributions/catalog/contribute.py
+++ contributions/catalog/contribute.py
@@ -7,6 +7,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from .db import get_db
 from .config import Config
+from ..api.controllers.contributions import client_contribution, db_contribution, coll_contribution, post, get, put, search, delete
 
 bp = Blueprint('contribute', __name__, url_prefix='/contribute')
 
@@ -34,26 +35,10 @@ def home():
 
     return render_template('contribute/home.html')
 
-@bp.route('/create', methods=('GET', 'POST'))
+@bp.route('/create', methods=('POST'))
 def create():
-    # if request.method == 'POST':
-    #     title = request.form['title']
-    #     body = request.form['body']
-    #     error = None
-    #
-    #     if not title:
-    #         error = 'Title is required.'
-    #
-    #     if error is not None:
-    #         flash(error)
-    #     else:
-    #         db = get_db()
-    #         db.execute(
-    #             'INSERT INTO post (title, body, author_id)'
-    #             ' VALUES (?, ?, ?)',
-    #             (title, body, g.user['id'])
-    #         )
-    #         db.commit()
-    #         return redirect(url_for('blog.index'))
-
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        post()
     return render_template('contribute/contribute.html')
