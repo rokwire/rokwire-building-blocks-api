f7792c889e1a49b0c63c0d47dee33e6cc5871565
Kim <ywkim@illinois.edu>
2020-09-18 09:21:09 -0500

    modified changelog

--- auth-middleware-test-svc/flaskapp.py
+++ auth-middleware-test-svc/flaskapp.py
@@ -12,7 +12,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-# use this for installed auth library
 import flask
 import auth_middleware
 from dotenv import load_dotenv
