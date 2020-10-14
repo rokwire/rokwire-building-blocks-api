795f4f42b6ead3d0050982d0b934a8bfd549667c
Yong Wook Kim <ywkim@illinois.edu>
2020-10-14 15:54:48 -0500

    removed unnecessary method

--- contributions/api/utils/query_params.py
+++ contributions/api/utils/query_params.py
@@ -14,20 +14,6 @@
 
 from bson import ObjectId
 
-
-def format_query(args, query):
-    query_parts = []
-
-    if args.get('email'):
-        query_parts.append({'email': {'$eq': args.get('email')}})
-    if args.get('phone'):
-        query_parts.append({'phone': {'$eq': args.get('phone')}})
-
-    if query_parts:
-        query['$and'] = query_parts
-
-    return query
-
 def format_query_contribution(args, query):
     query_parts = []
 
