2a9eafb2b061a981941d3b692af32098fb8bc0cd
wenjzhuu <wenjzhuu@gmail.com>
2020-11-30 13:18:43 -0600

    clean up old code

--- contributions/catalog/catalog_rest_service.py
+++ contributions/catalog/catalog_rest_service.py
@@ -1,22 +1,15 @@
-import json
+import logging
+from requests_oauthlib import OAuth2Session
 import logging
 import os
-import random
-import string
-from urllib.parse import urlencode
-from requests_oauthlib import OAuth2Session
-from uuid import uuid4
-import os
 from time import gmtime
-import requests
-from controllers.auth import bp as auth_bp
+
+from flask import Flask, redirect, url_for, render_template, request, session
+from requests_oauthlib import OAuth2Session
+
 from controllers.config import Config as cfg
 from controllers.contribute import bp as contribute_bp
 from db import init_app
-from flask import g, Flask, jsonify, redirect, url_for, render_template, request, make_response, render_template_string, \
-    session
-from flask import session as login_session
-from flask_dance.contrib.github import make_github_blueprint, github
 
 debug = cfg.DEBUG
 
@@ -45,33 +38,21 @@ app.config.from_object(cfg)
 init_app(app)
 app.register_blueprint(contribute_bp)
 
-app.config["GITHUB_CLIENT_ID"] = os.getenv("GITHUB_CLIENT_ID", "...")
-app.config["GITHUB_CLIENT_SECRET"] = os.getenv("GITHUB_CLIENT_SECRET", "...")
-app.secret_key = "supersekrit"
-blueprint = make_github_blueprint(
-    client_id=os.getenv("GITHUB_CLIENT_ID", "..."),
-    client_secret=os.getenv("GITHUB_CLIENT_SECRET", "..."),
-)
-app.register_blueprint(blueprint, url_prefix="/login")
-
-
-
-client_id = os.getenv("GITHUB_CLIENT_ID", "...")
-client_secret = os.getenv("GITHUB_CLIENT_SECRET", "...")
+client_id = os.getenv("CLIENT_ID", "NO ID")
+client_secret = os.getenv("CLIENT_SECRET", "NO SECRET")
 authorization_base_url = 'https://github.com/login/oauth/authorize'
 token_url = 'https://github.com/login/oauth/access_token'
+
 
 @app.route("/")
 def demo():
     """Step 1: User Authorization.
-
-    Redirect the user/resource owner to the OAuth provider (i.e. Github)
-    using an URL with a few key OAuth parameters.
     """
+    # print("Step 1: User Authorization")
     github = OAuth2Session(client_id)
     authorization_url, state = github.authorization_url(authorization_base_url)
 
-    # State is used to prevent CSRF, keep this for later.
+    # State is used to prevent CSRF.
     session['oauth_state'] = state
     return redirect(authorization_url)
 
@@ -81,34 +62,37 @@ def demo():
 @app.route("/callback", methods=["GET"])
 def callback():
     """ Step 3: Retrieving an access token.
-
-    The user has been redirected back from the provider to your registered
-    callback URL. With this redirection comes an authorization code included
-    in the redirect URL. We will use that to obtain an access token.
     """
-
+    # print("Step 3: Retrieving an access token")
     github = OAuth2Session(client_id, state=session['oauth_state'])
     token = github.fetch_token(token_url, client_secret=client_secret,
                                authorization_response=request.url)
 
-    # At this point you can fetch protected resources but lets save
-    # the token and show how this is done from a persisted token
-    # in /profile.
     session['oauth_token'] = token
-
     return redirect(url_for('.profile'))
-
 
 @app.route("/profile", methods=["GET"])
 def profile():
     """Fetching a protected resource using an OAuth 2 token.
+    and parse the user name to the templates to display.
     """
+    print("Fetching a protected resource using an OAuth 2 token")
     github = OAuth2Session(client_id, token=session['oauth_token'])
-    return jsonify(github.get('https://api.github.com/user').json())
+    resp = github.get('https://api.github.com/user')
+    return render_template('contribute/home.html', user=resp.json()["login"])
+
 
 # """
-# using Flask-dance
+# using Flask-dance for auth
 # """
+# app.config["GITHUB_CLIENT_ID"] = os.getenv("GITHUB_CLIENT_ID", "...")
+# app.config["GITHUB_CLIENT_SECRET"] = os.getenv("GITHUB_CLIENT_SECRET", "...")
+# app.secret_key = "supersekrit"
+# blueprint = make_github_blueprint(
+#     client_id=os.getenv("GITHUB_CLIENT_ID", "..."),
+#     client_secret=os.getenv("GITHUB_CLIENT_SECRET", "..."),
+# )
+# app.register_blueprint(blueprint, url_prefix="/login")
 # @app.route("/")
 # def index():
 #     if not github.authorized:
