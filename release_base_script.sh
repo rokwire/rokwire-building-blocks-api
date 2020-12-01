cfd07969cd2f4431273099cba819184584ad362c
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2020-12-01 12:55:49 -0600

    Update releae base script.

--- release_base_script.sh
+++ release_base_script.sh
@@ -9,6 +9,10 @@ if [[ "${GIT_BRANCH}" = "master" ]]; then
     VERSION=${VERSION:-"$(git describe --abbrev=0 --tags)"}
 elif [[ "${GIT_BRANCH}" = "develop" ]]; then
     VERSION="develop"
+elif [[ "${GIT_BRANCH}" = "group-events/master" ]]; then
+    VERSION=${VERSION:-"group-events-$(git describe --abbrev=0 --tags)"}
+elif [[ "${GIT_BRANCH}" = "group-events/develop" ]]; then
+    VERSION="group-events-develop"
 else
     exit 0
 fi
