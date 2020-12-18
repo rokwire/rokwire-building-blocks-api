c2e2bf2c9bdc82655459cefd552cac162656f230
Stephen Hurwit <10472810+shurwit@users.noreply.github.com>
2020-12-18 09:35:06 -0600

    Update profileservice/api/controllers/configs.py
    Co-authored-by: Sandeep Puthanveetil Satheesan <sandeep.ps4@gmail.com>

--- profileservice/api/controllers/configs.py
+++ profileservice/api/controllers/configs.py
@@ -22,7 +22,7 @@ load_dotenv()
 # this has to be have the file path to the folder for saving image files and other necessary files from rest service
 PROFILE_REST_STORAGE = os.getenv('REST_STORAGE', '/usr/src/app/rest')
 MONGO_PROFILE_URL = os.getenv('MONGO_PROFILE_URL', 'localhost:27017')
-MONGO_PII_URL = os.getenv('MONGO_PROFILE_URL', 'localhost:27017')
+MONGO_PII_URL = os.getenv('MONGO_PII_URL', 'localhost:27017')
 FLASK_APP = os.getenv('FLASK_APP', 'profile_rest_service')
 FLASK_ENV = os.getenv('FLASK_ENV', 'production')
 API_LOC = os.getenv('API_LOC', '../')
