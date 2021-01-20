18cb6f83c061a33d19e28c99344f3b057104e3eb
YONG WOOK KIM <ywkim312@users.noreply.github.com>
2021-01-20 17:37:32 -0600

    Task/602 create list endpoint for contributions (#604)
    * added contributions list
    
    * added list for contributions, capabilities, and talents
    
    * modified changelog
    
    * fixed yaml
    
    * changed listing method
    
    * Update CHANGELOG.md

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,11 +5,14 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Added
+- Add endpoints for listing contributions, capabilities, and talents. [#602](https://github.com/rokwire/rokwire-building-blocks-api/issues/602)
 
 ## [1.9.0] - 2020-12-17
 ### Fixed
 - Fix the error code to 400 on events missing fields query. [#448](https://github.com/rokwire/rokwire-building-blocks-api/pull/448)
 - Text changes to Contributions Catalog. [#601](https://github.com/rokwire/rokwire-building-blocks-api/issues/601)
+
 ### Added
 - Add capabilities dataset in contribution building block. [#401](https://github.com/rokwire/rokwire-building-blocks-api/issues/401)
 - Add contribution building block. [#406](https://github.com/rokwire/rokwire-building-blocks-api/issues/406)
--- contributions/api/utils/mongoutils.py
+++ contributions/api/utils/mongoutils.py
@@ -30,13 +30,65 @@ coll_contribution.create_index([("capabilities.name", ASCENDING)], background=Tr
 coll_contribution.create_index([("talents.name", ASCENDING)], background=True)
 
 """
-get query output json from query using search arguments
+get json of all the contributions list
 """
-def get_result(db_collection, query):
-    if not query:
+def list_contributions(db_collection):
+    db_data = db_collection.find({}, {'_id': False})
+    data_list = list(db_data)
+
+    if len(data_list) > 0:
+        data_dump = dumps(data_list)
+        if len(data_list) == 1: # remove blacket in the first and last character location
+            data_dump = data_dump[:-1]
+            data_dump = data_dump[1:]
+        json_load = json.loads(data_dump)
+
+        return json_load
+    else:
         return None
 
-    db_data = db_collection.find(query, {'_id': 0})
+"""
+get json of all the talent list
+"""
+def list_talents(db_collection):
+    db_data = db_collection.find({}, {'talents':1, '_id':0})
+    data_list = list(db_data)
+
+    if len(data_list) > 0:
+        data_dump = dumps(data_list)
+        if len(data_list) == 1: # remove blacket in the first and last character location
+            data_dump = data_dump[:-1]
+            data_dump = data_dump[1:]
+        json_load = json.loads(data_dump)
+
+        return json_load
+    else:
+        return None
+
+"""
+get json of all the capabilities list
+"""
+def list_capabilities(db_collection):
+    db_data = db_collection.find({}, {'capabilities':1, '_id':0})
+    data_list = list(db_data)
+
+    if len(data_list) > 0:
+        data_dump = dumps(data_list)
+        if len(data_list) == 1: # remove blacket in the first and last character location
+            data_dump = data_dump[:-1]
+            data_dump = data_dump[1:]
+        json_load = json.loads(data_dump)
+
+        return json_load
+    else:
+        return None
+
+def get_result(db_collection, query):
+    if not query:
+        db_data = db_collection.find({}, {'_id': 0})
+    else:
+        db_data = db_collection.find(query, {'_id': 0})
+
     data_list = list(db_data)
 
     if len(data_list) > 0:
