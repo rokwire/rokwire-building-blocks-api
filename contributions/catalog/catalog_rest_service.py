a237ad30ba112030ce0fcca0bbd785e64592d4f6
wenjzhuu <wenjzhuu@gmail.com>
2021-01-26 10:14:42 -0600

    remove print log

--- contributions/catalog/catalog_rest_service.py
+++ contributions/catalog/catalog_rest_service.py
@@ -35,7 +35,7 @@ app = Flask(__name__, instance_relative_config=True, static_url_path=staticpath,
             template_folder=template_dir)
 
 init_app(app)
-CORS(app, resources={r"/localhost:5050/*": {"origins": "*"}}, supports_credentials=True)
+# CORS(app, resources={r"/localhost:5050/*": {"origins": "*"}}, supports_credentials=True)
 app.register_blueprint(contribute_bp)
 
 # read configs variable from .env file
@@ -46,6 +46,7 @@ client_id = config["CLIENT_ID"]
 client_secret = config["CLIENT_SECRET"]
 authorization_base_url = config["authorization_base_url"]
 token_url = config["token_url"]
+contribution_url = config["CONTRIBUTION_BUILDING_BLOCK_URL"]
 
 
 @app.route("/")
@@ -55,10 +56,10 @@ def index():
     print("Step 1: User Authorization")
     github = OAuth2Session(client_id)
     authorization_url, state = github.authorization_url(authorization_base_url)
-    print(authorization_url, state)
+    # print(authorization_url, state)
     # State is used to prevent CSRF.
     session['oauth_state'] = state
-    print(session)
+    # print(session)
     return redirect(authorization_url)
 
 
@@ -73,7 +74,7 @@ def callback():
     token = github.fetch_token(token_url, client_secret=client_secret,
                                authorization_response=request.url)
 
-    print(token)
+    # print(token)
     session['oauth_token'] = token
     return redirect(url_for('.profile'))
 
@@ -85,7 +86,7 @@ def profile():
     # print("Fetching a protected resource using an OAuth 2 token")
     github = OAuth2Session(client_id, token=session['oauth_token'])
     resp = github.get('https://api.github.com/user')
-    print(resp.json())
+    # print(resp.json())
     session["username"] = resp.json()["login"]
     session['name'] = resp.json()["name"]
     return render_template('contribute/home.html', user=session["name"])
