79a4439b5301c65869bdddc4b7c31e8b757595ec
Yong Wook Kim <ywkim@illinois.edu>
2021-03-22 12:24:22 -0500

    added request_oauthlib in setup.py

--- lib/auth-middleware/setup.py
+++ lib/auth-middleware/setup.py
@@ -30,6 +30,7 @@ setup(
         'PyJWT>=1.7.1',
         'requests>=2.22.0',
         'cryptography>=2.7',
+        'requests_oauthlib>=1.3.0'
         # I would require flask here, but I want to use the version that the
         # "calling" app uses, and I don't see a way to guarantee that pip
         # installs the dependency in the correct order (eg. "calling app" first
