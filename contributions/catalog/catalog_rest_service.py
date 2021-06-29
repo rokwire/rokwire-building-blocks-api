ea76a560612c1a19e0854dcbc95d32f6df6dd3b6
Yong Wook Kim <ywkim@illinois.edu>
2021-06-29 11:50:43 -0500

    saving purpose

--- contributions/catalog/catalog_rest_service.py
+++ contributions/catalog/catalog_rest_service.py
@@ -1,11 +1,3 @@
-import os, sys
-# sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))))
-# sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))))
-# sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
-# sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
-# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
 import logging
 import os
 from time import gmtime
