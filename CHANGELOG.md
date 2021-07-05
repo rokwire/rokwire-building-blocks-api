3588de33623a773acdbb263b2591f84ee1e84596
Bing Zhang <bing@illinois.edu>
2021-07-05 11:07:55 -0500

    add group authorization to DELETE /events/{event_id}/images/{image_id} endpoint

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [Unreleased]
 ### Added
 - Add group authorization to GET /events/{event_id}/images endpoint.[#716](https://github.com/rokwire/rokwire-building-blocks-api/issues/716)
+- Add group authorization to DELETE /events/{event_id}/images/{image_id} endpoint.[#720](https://github.com/rokwire/rokwire-building-blocks-api/issues/720)
 
 ## [1.10.0] - 2021-06-29
 ### Added
--- eventservice/api/controllers/events.py
+++ eventservice/api/controllers/events.py
@@ -552,6 +552,25 @@ def images_put(event_id, image_id):
 def images_delete(event_id, image_id):
     auth_middleware.authorize(auth_middleware.ROKWIRE_EVENT_WRITE_GROUPS)
 
+    group_memberships = list()
+    try:
+        _, group_memberships = get_group_memberships()
+    except Exception as ex:
+        __logger.exception(ex)
+        abort(500)
+
+    db = None
+    event = None
+    try:
+        db = get_db()
+        event = db['events'].find_one({'_id': ObjectId(event_id)}, {'_id': 0})
+    except Exception as ex:
+        __logger.exception(ex)
+        abort(500)
+
+    if not check_group_event_admin_access(event, group_memberships):
+        abort(401)
+
     msg = "[delete image]: event id %s, image id: %s" % (str(event_id), str(image_id))
     try:
         S3EventsImages().delete(event_id, image_id)
