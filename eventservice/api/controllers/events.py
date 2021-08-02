ec66f3705f3d26dad3f15d4009a62605c18e7fe3
Bing Zhang <bing@illinois.edu>
2021-08-02 11:58:57 -0500

    group auth to post image

--- eventservice/api/controllers/events.py
+++ eventservice/api/controllers/events.py
@@ -472,6 +472,25 @@ def _get_imagefiles_result(query):
 def images_post(event_id):
     auth_middleware.authorize(auth_middleware.ROKWIRE_EVENT_WRITE_GROUPS)
 
+    group_memberships = list()
+    try:
+        _, group_memberships = get_group_memberships()
+    except Exception as ex:
+        __logger.exception(ex)
+        abort(500)
+
+    db = None
+    event = None
+    try:
+        db = get_db()
+        event = db['events'].find_one({'_id': ObjectId(event_id)}, {'_id': 0})
+    except Exception as ex:
+        __logger.exception(ex)
+        abort(500)
+
+    if not check_group_event_admin_access(event, group_memberships):
+        abort(401)
+
     tmpfile = None
     try:
         file = request.files.get('file')
