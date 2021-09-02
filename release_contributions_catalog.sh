c8af2d84aa63f01504d8c77b0788764800519981
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2021-09-02 10:21:30 -0500

    Update release_contributions_catalog.sh

--- release_contributions_catalog.sh
+++ release_contributions_catalog.sh
@@ -2,7 +2,7 @@
 
 source release_base_script.sh
 
-###### CONTRIBUTIONS BUILDING BLOCK ######
+###### CONTRIBUTIONS CATALOG ######
 docker build --pull -f contributions/Dockerfile -t ${PROJECT_NAME}/contributions-catalog:${VERSION} .
 docker tag ${PROJECT_NAME}/contributions-catalog:${VERSION} 779619664536.dkr.ecr.us-east-2.amazonaws.com/${PROJECT_NAME}/contributions-catalog:${VERSION}
 docker push 779619664536.dkr.ecr.us-east-2.amazonaws.com/${PROJECT_NAME}/contributions-catalog:${VERSION}
