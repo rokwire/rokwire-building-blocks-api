128c9fa83a703fce39342dfd282fcc4708015359
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2021-09-27 18:08:31 -0500

    Fix Catalog release script.

--- release_contributions_catalog.sh
+++ release_contributions_catalog.sh
@@ -3,6 +3,6 @@
 source release_base_script.sh
 
 ###### CONTRIBUTIONS CATALOG ######
-docker build --pull -f contributions/Dockerfile -t ${PROJECT_NAME}/contributions-catalog:${VERSION} .
+docker build --pull -f contributions/catalog/Dockerfile -t ${PROJECT_NAME}/contributions-catalog:${VERSION} .
 docker tag ${PROJECT_NAME}/contributions-catalog:${VERSION} 779619664536.dkr.ecr.us-east-2.amazonaws.com/${PROJECT_NAME}/contributions-catalog:${VERSION}
 docker push 779619664536.dkr.ecr.us-east-2.amazonaws.com/${PROJECT_NAME}/contributions-catalog:${VERSION}
