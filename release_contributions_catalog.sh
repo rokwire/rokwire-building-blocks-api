340409e38a0d77827a3605c9a015c9ea34c34bd2
Minu Mathew <minumpmathews@gmail.com>
2021-11-05 13:28:50 -0500

    changes to dockerfile and release script
    changed catalog release script to fetch catalog Dockerfile and added build args

--- release_contributions_catalog.sh
+++ release_contributions_catalog.sh
@@ -9,6 +9,6 @@ gitsha=$(git rev-parse --short HEAD)
 
 
 ###### CONTRIBUTIONS CATALOG ######
-docker build --pull -f contributions/Dockerfile -t ${PROJECT_NAME}/contributions-catalog:${VERSION} --build-arg GIT_TAG=$gittag --build-arg GIT_SHA=$gitsha .
+docker build --pull -f contributions/catalog/Dockerfile -t ${PROJECT_NAME}/contributions-catalog:${VERSION} --build-arg GIT_TAG=$gittag --build-arg GIT_SHA=$gitsha .
 docker tag ${PROJECT_NAME}/contributions-catalog:${VERSION} 779619664536.dkr.ecr.us-east-2.amazonaws.com/${PROJECT_NAME}/contributions-catalog:${VERSION}
 docker push 779619664536.dkr.ecr.us-east-2.amazonaws.com/${PROJECT_NAME}/contributions-catalog:${VERSION}
