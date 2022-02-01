92fe1f2705bcbba1c446900f9270cc7e24f59be1
Minu Mathew <minumpmathews@gmail.com>
2022-02-01 13:48:16 -0600

    added explicit if statement

--- contributions/catalog/models/capability_utilities.py
+++ contributions/catalog/models/capability_utilities.py
@@ -67,7 +67,8 @@ def to_capability(d):
                     capability_list[i]["isOpenSource"] = False
                 d[k][i] = capability_list[i]["isOpenSource"]
             elif "sourceRepoUrl" in k:
-                capability_list[i]["sourceRepoUrl"] = v[i]
+                if capability_list[i]["isOpenSource"]:
+                    capability_list[i]["sourceRepoUrl"] = v[i]
             elif "deploymentDetails_" in k:
                 name = k.split("deploymentDetails_")[-1]
                 capability_list[i]["deploymentDetails"][name] = v[i]
