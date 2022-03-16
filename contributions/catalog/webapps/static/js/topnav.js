51308f6f9e32fc70a4daae175e78f8254bcec41e
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2022-03-16 12:16:10 -0500

    Committing for saving purposes.

--- contributions/catalog/webapps/static/js/topnav.js
+++ contributions/catalog/webapps/static/js/topnav.js
@@ -1,29 +1,30 @@
-function start() {
-    var user_name = "{{user}}";
-    if (user_name == "None") {
-            user_name = "";
-    }
-    // user is None here
-    // document.getElementById("demo").innerHTML = "{{user}}"
-    // var user_name = $('#nav-user-data').data("user");
-    //console.log(user_name)
-    // alert(user_name)
-    var nav_login = document.getElementById('nav_login');
-    var nav_greet = document.getElementById('nav_greeting')
-    var nav_user = document.getElementById('user_profile')
-    var submit_btn = document.getElementById('submit_contribution')
-    if (user_name.length > 0) {
-        nav_login.style.display = 'none';
-        nav_greet.style.display = 'block';
-        nav_user.style.display = 'block';
-        submit_btn.style.display = 'block';
-    } else {
-        nav_login.style.display = 'block';
-        nav_greet.style.display = 'none';
-        nav_user.style.display = 'block';
-        submit_btn.style.display = 'none';
-    }
-}
+// function start() {
+//     var user_name = "{{ user }}";
+//     if (user_name == "None") {
+//             user_name = "";
+//     }
+//     console.log(user_name);
+//     // user is None here
+//     // document.getElementById("demo").innerHTML = "{{user}}"
+//     // var user_name = $('#nav-user-data').data("user");
+//     //console.log(user_name)
+//     // alert(user_name)
+//     var nav_login = document.getElementById('nav_login');
+//     var nav_greet = document.getElementById('nav_greeting')
+//     var nav_user = document.getElementById('user_profile')
+//     var submit_btn = document.getElementById('submit_contribution')
+//     if (user_name !== undefined && user_name.length > 0) {
+//         nav_login.style.display = 'none';
+//         nav_greet.style.display = 'block';
+//         nav_user.style.display = 'block';
+//         submit_btn.style.display = 'block';
+//     } else {
+//         nav_login.style.display = 'block';
+//         nav_greet.style.display = 'none';
+//         nav_user.style.display = 'block';
+//         submit_btn.style.display = 'none';
+//     }
+// }
 
 var user_icon = document.getElementById('user_profile');
 user_icon.addEventListener('click', function () {
