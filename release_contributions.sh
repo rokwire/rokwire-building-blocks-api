d89159da61e27bcb607418080348e64c3d50cf57
Minu Mathew <minumpmathews@gmail.com>
2022-05-26 09:31:43 -0500

    removed no-cache from docker build

--- release_contributions.sh
+++ release_contributions.sh
@@ -3,6 +3,6 @@
 source release_base_script.sh
 
 ###### CONTRIBUTIONS BUILDING BLOCK ######
-docker build --no-cache --pull -f contributions/Dockerfile -t ${PROJECT_NAME}/contributions-building-block:${VERSION} .
+docker build --pull -f contributions/Dockerfile -t ${PROJECT_NAME}/contributions-building-block:${VERSION} .
 docker tag ${PROJECT_NAME}/contributions-building-block:${VERSION} 779619664536.dkr.ecr.us-east-2.amazonaws.com/${PROJECT_NAME}/contributions-building-block:${VERSION}
 docker push 779619664536.dkr.ecr.us-east-2.amazonaws.com/${PROJECT_NAME}/contributions-building-block:${VERSION}
