7a2f8aa4d5dd545b2b2427db9fec96daa197b8b7
Minu Mathew <minumpmathews@gmail.com>
2022-06-02 14:05:25 -0500

    updated list reviewers check None

--- contributions/api/controllers/contributions.py
+++ contributions/api/controllers/contributions.py
@@ -203,7 +203,7 @@ def post(token_info):
         if notification_enabled:
             list_reviewers = mongoutils.list_reviewers()
             # check for empty collection
-            if list_reviewers is not None:
+            if list_reviewers:
                 for reviewer in list_reviewers:
                     send_email_new_contribution(reviewer['githubUsername'], contribution_name)
 
